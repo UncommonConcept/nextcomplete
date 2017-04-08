0fb379afef870fe8b3aaa6df7c5dcd42d73a3809
David <dcunkefer@gmail.com>
2017-04-08 11:13:20 -0500

    react form added

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,42 @@
 import Layout from '../components/MyLayout.js'
-import { aboutMatt, aboutService } from './content.js';
+import { aboutMatt, aboutService, poop } from './content.js';
+import { Form, Text } from 'react-form';
+
+
+const myForm = (
+  <Form
+    onSubmit={(values) => {
+      console.log('Success!', values)
+    }}
+    validate={({ name }) => {
+      return {
+        name: !name ? 'A name is required' : undefined
+      }
+    }}
+  >
+    {({submitForm}) => {
+      return (
+        <form onSubmit={submitForm}>
+          <Text field='name' placeholder = "name" />
+          <Text field='email' placeholder = "email" />
+          <Text field='date' placeholder = "date" />
+          <Text field='time' placeholder = "time" />
+          <Text field='issue' placeholder = "issue" />
+          <button type='submit'>Submit</button>
+        </form>
+      )
+    }}
+  </Form>
+)
+
 
 export default () => (
     <Layout>
         <h2>About Time </h2>
        <p>
-           {aboutMatt}
+           {aboutMatt()}
+           {myForm}
+           {poop()}
         </p>
     </Layout>
 )
--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,29 @@
 import Layout from '../components/MyLayout.js'
+import { Form, Text } from 'react-form'
+
+
+const myForm = (
+  <Form
+    onSubmit={(values) => {
+      console.log('Success!', values)
+    }}
+    validate={({ name }) => {
+      return {
+        name: !name ? 'A name is required' : undefined
+      }
+    }}
+  >
+    {({submitForm}) => {
+      return (
+        <form onSubmit={submitForm}>
+          <Text field='name' />
+          <button type='submit'>Submit</button>
+        </form>
+      )
+    }}
+  </Form>
+)
+
 
 
 export default () => (
@@ -7,5 +32,6 @@ export default () => (
        <p>
 
         </p>
+        <myForm />
     </Layout>
 )
