4e8e58b755517b149baa09b2f235228b9e17f55a
David <dcunkefer@gmail.com>
2017-04-10 12:52:48 -0500

    took out home and made title href

--- components/NavBar.js
+++ components/NavBar.js
@@ -9,17 +9,14 @@ const NavBar = () => (
     <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
         <header className="mdl-layout__header">
             <div className="mdl-layout__header-row">  
-            <span className="mdl-layout-title">Complete AC Air and Heat Services</span>
+            <Link className="mdl-layout-title" href="/"><span >Complete AC Air and Heat Services</span></Link>
             <div className="mdl-layout-spacer"></div>
             <nav className="mdl-navigation mdl-layout--large-screen-only">
-                <Link href="/">
-                    <a className="mdl-navigation__link">Home</a>
-                </Link>
+                  <Link href="/contact">
+                    <a className="mdl-navigation__link">Service Request</a>
+                 </Link>
                  <Link href="/about">
                     <a className="mdl-navigation__link">About</a>
-                 </Link>
-                  <Link href="/contact">
-                    <a className="mdl-navigation__link">Servie Request</a>
                  </Link>
             </nav>
             </div>
