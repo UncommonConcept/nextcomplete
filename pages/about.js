8b8d01e8682dbd2e391f4a8678978e33e6a90ed1
David <dcunkefer@gmail.com>
2017-04-14 23:13:20 -0500

    images pulling from s3 bucket

--- pages/about.js
+++ pages/about.js
@@ -12,6 +12,9 @@ export default () => (
        <p>{aboutService}</p>
        <div className= "mdl-grid">
            <div className="mdl-cell mdl-cell--6-col">
+               <img src={ "https://s3-us-west-2.amazonaws.com/completeair/nj-air-conditioning-repair-service.jpg"}  width={200} height={200} />
+   
+       
            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec leo dolor, eleifend vitae urna varius, viverra rhoncus lectus. Morbi finibus arcu nec odio bibendum congue. In vitae elementum felis, eu ultrices nisl. Nullam vitae dui nulla. Mauris vitae gravida diam. Donec quis ullamcorper urna. Aliquam tristique orci at dapibus imperdiet.
 Curabitur pellentesque felis non ante pellentesque maximus. Integer condimentum nunc eu augue facilisis lobortis nec ut augue. Duis at libero neque. Curabitur ut risus rhoncus, pharetra odio eget, blandit lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quis nulla leo. Maecenas pellentesque sit amet lectus quis consequat. Etiam consequat, ante quis maximus consectetur, dolor metus consequat lacus, vel mollis justo erat non orci. Cras accumsan nisl a pulvinar porta. Nam placerat elit eu placerat laoreet. In elit justo, auctor a volutpat eu, auctor et eros. Aliquam velit ante, sollicitudin nec metus eget, ornare finibus ipsum. Sed interdum pellentesque enim, vitae fermentum felis feugiat ut. Maecenas maximus nisl sit amet elementum tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempor nulla id volutpat porttitor.
 Fusce eu ante sapien. Cras at egestas quam, a vulputate enim. Sed elementum vel augue porttitor maximus. Phasellus hendrerit interdum sem quis placerat. Sed non diam ut orci pretium consectetur. Aenean vitae lacinia risus. Sed cursus purus ligula, ac consequat enim consectetur at.
