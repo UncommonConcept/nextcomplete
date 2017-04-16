b5d02f7b0b9fd5415a8582e4c6ea8b9369d4ae51
David <dcunkefer@gmail.com>
2017-04-15 22:27:28 -0500

    added image need background style

--- components/MyLayout.js
+++ components/MyLayout.js
@@ -2,15 +2,23 @@ import Header from './Header'
 import NavBar from './NavBar'
 
 const layoutStyle = {
-  // margin: 20,
-  // padding: 20,
-  // border: '1px solid #DDD'
+ //backgroundImage: 'url(https://s3-us-west-2.amazonaws.com/completeair/service-ac-repair-big.jpg)',
+		//backgroundPosition: center,
+		//	backgroundRepeat:  repeat,
+		//	backgroundAttachment: fixed,
+//	 backgroundImage:{ resizeMode: 'cover'},
+
+
 }
 
 const Layout = (props) => (
   <div style={layoutStyle}>
+    
     <Header />
     <NavBar />
+    <div id="background">
+      <img src="https://s3-us-west-2.amazonaws.com/completeair/service-ac-repair-big.jpg" class="stretch" alt="" />
+    </div>
     {props.children}
   </div>
 )
