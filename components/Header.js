1ee5f39331e013e6d3e971f60ee315b044543032
David <dcunkefer@gmail.com>
2017-04-18 21:44:46 -0500

    bootstrap nav bar replaces material nav

--- components/Header.js
+++ components/Header.js
@@ -12,7 +12,7 @@ const Header = () => (
           <title>Plano McKinney Complete Air and Heat</title>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
-          <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-blue.min.css"/>
+          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
           <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
           <script type="text/javascript" defer src="https://cdn.emailjs.com/dist/email.min.js"></script>
           <script src='https://www.google.com/recaptcha/api.js'></script>
--- components/NavBar.js
+++ components/NavBar.js
@@ -5,7 +5,34 @@ const linkStyle = {
   marginRight: 15
 }
 
-const NavBar = () => (
+const NavBar = () =>(
+    <nav className="navbar navbar-default">
+        <div className="container-fluid">
+            <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand" href="/">Complete Air and Heat Service</a>
+            </div>
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul className="nav navbar-nav">
+                    <Link prefetch href="/contact">
+                        <li className="nada"><a href="#">Service Request <span className="sr-only">(current)</span></a></li>
+                    </Link>
+                    <Link prefetch href="/about">
+                        <li><a href="/">About</a></li>
+                    </Link>
+                </ul>
+            </div>
+        </div>
+</nav>
+
+)
+
+const NavBarPoop = () => (
     <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
         <header className="mdl-layout__header">
             <div className="mdl-layout__header-row">  
