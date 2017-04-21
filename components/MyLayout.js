83c670bf4bf822b32b438575f160411596e3ab15
David <dcunkefer@gmail.com>
2017-04-21 00:48:47 -0500

    list of cities in layout

--- components/MyLayout.js
+++ components/MyLayout.js
@@ -27,6 +27,18 @@ const Layout = (props) => (
     </div>
     {props.children}
     {serviceRequestForm}
+    <div className = "bootstrap something">
+      Complete AC and Heat is a licensed HVAC contractor and air conditioning company offering AC repair, 
+           Air Conditioning tune-up, and air conditioning replacement services in Southlake, TX, Keller, TX, 
+           Coppell, TX, Colleyville, TX, 
+           Grapevine, TX, Plano, TX, 
+           Arlington, TX, Frisco, TX, Dallas, TX, Addison, TX, 
+           Flower Mound, TX, Irving, TX, McKinney, TX, 
+           Arlington, TX, Euless, TX, Denton, TX, 
+           Highland Village, TX, Grand Prairie, TX, 
+           Farmers Branch, TX, Lewisville, TX, Mesquite, Allen, Whylie, Garland, Sachse,
+           Richardson, Lucas, Parker, Princeton, Melissa, Little Elm, 
+           Rockwall, Rowlett, Murphy, Carrollton.</div>
   </div>
 )
 
