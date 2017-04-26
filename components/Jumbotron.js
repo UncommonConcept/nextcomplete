3e69041615098be519de1ec3bce63bac1574f996
David <dcunkefer@gmail.com>
2017-04-26 14:51:40 -0500

    nav bar and jumbo updated need content

--- components/Jumbotron.js
+++ components/Jumbotron.js
@@ -9,7 +9,7 @@ const Jumbotron = ({ messages }) => {
   return (
     <div className="jumbotron">
       <div className="container">
-        <h1 className="display-3">Hello, world!</h1>
+        <h1 className="display-3">Complete AC & Heat</h1>
         <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
         <p><a className="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
         <p>{messages}</p>
