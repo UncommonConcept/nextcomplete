ec741fd4ea2910cdef741619f98c54cbf8b1c5f2
David <dcunkefer@gmail.com>
2017-05-02 01:10:42 -0500

    need to change state of show form

--- components/serviceRequestForm/serviceRequestForm.js
+++ components/serviceRequestForm/serviceRequestForm.js
@@ -55,21 +55,18 @@ class RequestForm extends PureComponent {
       ? { userAgent: req.headers['user-agent'] }
       : { userAgent: navigator.userAgent }
   }
+  constructor (props) {
+      super(props)
+      this.state = {
+        showForm: true,
+      };
+    }
 
   render () {   
+    
     return (
       <div>
-   
-
-            {serviceRequestForm}
-          {/*<button
-                className="g-recaptcha"
-                data-sitekey={emailJs.siteCaptcha}
-                data-callback="YourOnSubmitFn">
-                Submit Captura
-                </button>*/}
-
-  
+            {this.state.showForm ? serviceRequestForm : null}
       </div>
     )
   }
@@ -77,71 +74,3 @@ class RequestForm extends PureComponent {
 
 export default RequestForm;
 
-
-
-
-
-
-
-
-
-/*
-export const serviceRequestForm = (
-  <div>
-  <Form
-    onSubmit={(values) => {
-      console.log('Success!', values)
-      //uncomment the below two lines to activate email delivery
-       // emailjs.init(emailJs.userLogin);
-      //  emailjs.send("default_service","template_pWhzP98u",{name: "James",message_html: "Check this out!"});
-
-          }}
-          validate={({ name }) => {
-            return {
-              name: !name ? 'A name is required' : undefined
-            }
-          }}
-  >
-    {({submitForm}) => {
-      return (
-        <form className="form-appointment" onSubmit={submitForm}>
-          <Text className="form" field='name' placeholder = "name" />
-          <Text field='email' placeholder = "email" />
-          <div className ="form-group" >
-            <Textarea field='issue' placeholder = "issue" rows="5" />
-          </div>
-            <DatePicker />  
-          <button
-                className="g-recaptcha"
-                data-sitekey={emailJs.siteCaptcha}
-                data-callback="YourOnSubmitFn">
-                Submit Captura
-                </button>
-          <button type='submit'>Submit</button>
-        </form>
-      )
-    }}
-  </Form>
-  </div>
-)
-
-
-/*
-export default class extends React.Component {
-  static async getInitialProps ({ req }) {
-    return req
-      ? { userAgent: req.headers['user-agent'] }
-      : { userAgent: navigator.userAgent }
-  }
-
-  render () {   
-      console.log("service reque", serviceRequestForm)
-    return (
-    <div>
-      Hello World {this.props.userAgent}
-      {serviceRequestForm}
-    </div>
-    )
-  }
-}
-*/
