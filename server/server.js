45d24d78de0bb010c90483a44ceedc426ca3d5c0
David <dcunkefer@gmail.com>
2017-05-15 17:07:59 -0500

    server port to 80

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.prepare().then(() => {
     return handle(req, res);
   });
 
-  server.listen(3000, (err) => {
+  server.listen(80, (err) => {
     if (err) throw err;
     console.log('> Ready on http://localhost:3000');
   });
